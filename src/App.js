9ef900ab06a3d5e43e8c04af27316a3c39c15ae2
HaiGentle <70798976+HaiGentle@users.noreply.github.com>
2021-03-31 16:26:42 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import { useEffect, useRef, useState } from "react";
+import "./styles.css";
+
+const fomatTime = (second) => {
+  const minuteString = `${Math.floor(second / 60)}`.padStart(2, "0");
+  const secondString = `${second % 60}`.padStart(2, "0");
+  return `${minuteString}:${secondString}`;
+};
+
+export default function App() {
+  const [time, setTime] = useState({
+    second: 0,
+    act: false, //false == pause and true == play
+    stoped: false
+  });
+  const inputMinuteRef = useRef();
+  const inputSecondRef = useRef();
+  const timeRef = useRef(); //ref innerHTML
+  const timeCurentRef = useRef(); //ref save current time
+
+  useEffect(() => {
+    let timeInterval,
+      currentSecond = time.second;
+    if (time.act && currentSecond > 0) {
+      timeInterval = setInterval(() => {
+        currentSecond -= 1;
+        timeCurentRef.current = currentSecond;
+        timeRef.current.innerHTML = fomatTime(currentSecond);
+      }, 1000);
+    }
+
+    return () => {
+      if (timeInterval) {
+        clearInterval(timeInterval);
+      }
+    };
+  }, [time.act]);
+
+  const handlePlayPauseClick = () => {
+    if (inputMinuteRef.current.value && inputSecondRef.current.value) {
+      const initSecond =
+        inputMinuteRef.current.value * 60 +
+        parseInt(inputSecondRef.current.value, 10);
+      const second = time.stoped
+        ? initSecond
+        : timeCurentRef.current || initSecond;
+      setTime((prev) => ({ second: second, act: !prev.act, stoped: false }));
+    }
+  };
+
+  const handleStopClick = () => {
+    if (inputMinuteRef.current.value && inputSecondRef.current.value) {
+      const initSecond =
+        inputMinuteRef.current.value * 60 +
+        parseInt(inputSecondRef.current.value, 10);
+
+      timeRef.current.innerHTML = fomatTime(initSecond);
+      setTime({
+        second: initSecond,
+        act: false,
+        stoped: true
+      });
+    }
+  };
+
+  const handleClearClick = () => {
+    inputMinuteRef.current.value = "";
+    inputSecondRef.current.value = "";
+    timeCurentRef.current = null;
+    setTime({
+      second: 0,
+      act: false,
+      stoped: false
+    });
+  };
+
+  return (
+    <div className="App">
+      Minute
+      <input ref={inputMinuteRef} />
+      Second
+      <input ref={inputSecondRef} />
+      {time.stoped && <button onClick={handleClearClick}>Clear</button>}
+      <h1 ref={timeRef}>{fomatTime(time.second)}</h1>
+      {time.act ? (
+        <button onClick={handlePlayPauseClick}>pause</button>
+      ) : (
+        <button onClick={handlePlayPauseClick}>play</button>
+      )}
+      <button onClick={handleStopClick}>stop</button>
+    </div>
+  );
+}
